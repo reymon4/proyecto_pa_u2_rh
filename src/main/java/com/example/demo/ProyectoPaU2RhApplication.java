9fe258e0a6571827733829f6217dae51b9265545
Reymon <rahidalgob@uce.edu.ec>
2023-01-03 19:41:49 -0500

    Intro U2

---
+++ src/main/java/com/example/demo/ProyectoPaU2RhApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2RhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2RhApplication.class, args);
+	}
+
+}
