aabad4b62280767e3f9a6209a438951f4541d1ef
Reymon <rahidalgob@uce.edu.ec>
2023-01-30 19:38:11 -0500

    ManyToMany

--- src/main/java/com/example/demo/ProyectoPaU2RhApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU2RhApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,20 +18,21 @@ import com.example.demo.uce.modelo.Empleado;
 import com.example.demo.uce.modelo.Estudiante;
 import com.example.demo.uce.modelo.Habitacion;
 import com.example.demo.uce.modelo.Hotel;
+import com.example.demo.uce.modelo.Libro;
+import com.example.demo.uce.modelo.Autor;
 import com.example.demo.uce.service.ICiudadanoSer;
 import com.example.demo.uce.service.IEmpleadoSer;
 import com.example.demo.uce.service.IEstudianteService;
 import com.example.demo.uce.service.IHabitacionService;
 import com.example.demo.uce.service.IHotelService;
+import com.example.demo.uce.service.ILibroSer;
 
 @SpringBootApplication
 public class ProyectoPaU2RhApplication implements CommandLineRunner{
 
 	@Autowired
-	private IHotelService hotelService;
-	
-	@Autowired
-	private IHabitacionService habService;
+	private ILibroSer libroSer;
+
 ;
 	
 	public static void main(String[] args) {
@@ -40,44 +43,34 @@ public class ProyectoPaU2RhApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		//TODO Auto-generated method stub
 
-
-		Hotel h1 = new Hotel();
-		h1.setNombre("Casa Gangotena");
-		h1.setDireccion("Plaza Grande");
+		Libro l1 = new Libro();
+		l1.setNombre("Análisis númerico");
+		l1.setEditorial("Calvache");
+		
+		Set<Autor> autores = new HashSet<>();
+		
+		Autor a1 = new Autor();
+		a1.setNombre("Oscar");
 	
-	
-		//this.hotelService.insertar(h1);
-		Habitacion ha1 = new Habitacion();
-		ha1.setNumero("A001");
-		ha1.setHotel(h1);
-		Habitacion ha2 = new Habitacion();
-		ha2.setNumero("A002");
-		ha2.setHotel(h1);
-		Habitacion ha3 = new Habitacion();
-		ha3.setNumero("A003");
-		ha3.setHotel(h1);
+		Autor a2 = new Autor();
+		a2.setNombre("David");
 		
-		List<Habitacion> habitaciones = new ArrayList<Habitacion>();
-		habitaciones.add(ha1);
-		habitaciones.add(ha2);
-		habitaciones.add(ha3);
+		Autor a3 = new Autor();
+		a3.setNombre("Jimmy");
 		
-		h1.setHabitaciones(habitaciones);
-		//this.hotelService.actualizar(h1);
 		
-		Hotel h2 = new Hotel();
-		h2.setNombre("Marriot");
-		h2.setDireccion("Av. Patria");
-//		h2.setHabitaciones(habitaciones);
-		//this.hotelService.insertar(h2);
-		Hotel hFind = this.hotelService.buscar(1);
-		System.out.println(hFind.toString());
-		hFind.getHabitaciones().stream().forEach(x-> System.out.println(x.toString()));
-//		this.hotelService.eliminar(3);
-////		
-//		
-//		h2.setNombre("Hotel Centro");
-//		this.hotelService.actualizar(h2);
+		autores.add(a3);
+		autores.add(a2);
+		autores.add(a1);
+		
+		
+		
+		
+		l1.setAutores(autores);
+		this.libroSer.insertar(l1);
+		
+		
+		
 		System.out.println("Finish!");
 	}
 
